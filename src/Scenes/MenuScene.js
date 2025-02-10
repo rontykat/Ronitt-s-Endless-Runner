1a41d7c75b6b14b71c2f08b0a21d0585bc1271cb
rontykat <rontykat@gmail.com>
2025-02-10 05:34:21 -0800

    added instructions, everything good to go

--- src/Scenes/MenuScene.js
+++ src/Scenes/MenuScene.js
@@ -6,6 +6,9 @@ export class MenuScene extends Phaser.Scene {
         this.sound.play('pickup');
         this.add.text(300, 250, 'Endless Runner', { fontSize: '32px', fill: '#fff' });
         this.add.text(250, 300, 'Press SPACE or Click to Start', { fontSize: '24px', fill: '#fff' });
+        this.add.text(200, 400, 'Instructions: Click or Press Space to Jump Past the Meteors and Asteroids!', 
+            { fontSize: '12px', fill: '#fff' });
+        
         this.input.keyboard.once('keydown-SPACE', () => this.scene.start('GameScene'));
         this.input.once('pointerdown', () => this.scene.start('GameScene'));
     }
